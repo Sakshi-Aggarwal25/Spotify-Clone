c5dd5e03cac5b62462c9e9b816c2b9749439da94
Sakshi-Aggarwal25 <sakshi.aggarwal@comprotechnologies.com>
2021-01-29 18:07:38 +0530

    new file:   routes/api/authentication.js

---
+++ routes/api/authentication.js
@@ -0,0 +1,8 @@
+const isLoggedIn = (req, res, next) => {
+    if(req.isAuthenticated()){
+        return next()
+    }
+    return res.status(400).json({"statusCode" : 400, "message" : "not authenticated"})
+}
+
+module.exports = isLoggedIn;
